5244f9aedc13bf66a59b9018133acca214c5bf01
Dimitris Menounos <dmenounos@gmail.com>
2016-05-12 09:22:05 +0300

    ...

--- foobar-bin/src/main.cpp
+++ foobar-bin/src/main.cpp
@@ -1,12 +1,8 @@
-#include "foobar_bin.hpp"
 #include "FooBar.hpp"
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("PROJECT_VERSION: %f\n", PROJECT_VERSION);
-
 	FooBar fooBar;
 	fooBar.say();
-
 	return 0;
 }
